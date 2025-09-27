f34a12e3313f18fb7040804e10baef1e18f4e89c
Pathan Aliya <144582876+pathanaliya@users.noreply.github.com>
2025-09-27 19:13:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-//your JS code here. If required.
-let panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel');
+
 panels.forEach(panel => {
-	panel.addEventListener('click', () =>{
-		removeActiveClasses();
-		panel.classList.add("active");
-	})
-})
-function removeActiveClasses(){
-	panels.forEach(panel => {
-		panel.classList.remove("active");
-	})
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
 }
